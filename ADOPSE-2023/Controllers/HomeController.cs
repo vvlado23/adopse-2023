8ed142de345d4423984828cef4a577be3e1228f5
MarkosLeopardi <127454326+MarkosLeopardi@users.noreply.github.com>
2023-04-07 10:10:07 +0300

    Add files via upload

---
+++ ADOPSE-2023/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using LoginRegister.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LoginRegister.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+    }
+}
