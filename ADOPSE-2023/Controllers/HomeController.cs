922666b6396037277d7e8c1600715c01ff4af225
vvlado23 <43526651+vvlado23@users.noreply.github.com>
2023-05-03 22:21:44 +0300

    adds login page

---
+++ ADOPSE-2023/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ADOPSE_2023.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+    }
+}
--- ADOPSE-2023/Program.cs
+++ ADOPSE-2023/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!!!!");
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=LoginPage}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
-
-//
