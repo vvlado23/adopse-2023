a0efa47bfa941b79a7364b3743ce02c0a2a752d9
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2023-05-19 08:32:55 +0300

    api so far
    logical error (redirect uri mismatch)

--- ADOPSE-2023/Controllers/HomeController.cs
+++ ADOPSE-2023/Controllers/HomeController.cs
@@ -1,7 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ADOPSE_2023.Controllers
 {
+    public class calendarEvent
+    {
+        public string Summary { get; set; }
+        public string Organizer { get; set; }
+        public string Description { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -21,9 +33,54 @@ namespace ADOPSE_2023.Controllers
             return View();
         }
 
+        public List<calendarEvent> GoogleEvents = new List<calendarEvent>();
+        //static string[] scopes = { CalendarService.Scope.CalendarReadonly };
+
         public IActionResult HomePage()
         {
+            API();
+            ViewBag.EventList = GoogleEvents;
             return View();
+        }
+
+        public void API()
+        {
+            // Authenticate using OAuth 2.0
+            UserCredential credential;
+            using (var stream = new FileStream("Credential.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.FromStream(stream).Secrets,
+                    new[] { CalendarService.Scope.Calendar },
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            // Create the calendar service
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Ogma"
+            });
+
+            // Query the calendar events
+            var events = service.Events.List("primary").Execute().Items;
+
+            // Display the calendar events in your web app
+            foreach (var evt in events)
+            {
+                // Display the event details (title, start time, end time, etc.)
+                var calendarevt = new calendarEvent();
+                calendarevt.Organizer = evt.Organizer.Email;
+                calendarevt.Summary = evt.Summary;
+                calendarevt.StartTime = evt.Start.DateTimeRaw.ToString();
+                calendarevt.EndTime = evt.End.DateTimeRaw.ToString();
+                calendarevt.Description = evt.Description;
+                GoogleEvents.Add(calendarevt);
+            }
+
         }
     }
 }
