64d028d79c2f2187c4ab60e3e955425cdb3a6c37
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2023-05-20 00:40:11 +0300

    Update
    Still the error 400: redirect uri mismatch.

--- ADOPSE-2023/Controllers/HomeController.cs
+++ ADOPSE-2023/Controllers/HomeController.cs
@@ -1,19 +1,16 @@
-﻿using Google.Apis.Auth.OAuth2;
+﻿using ADOPSE_2023.Models;
+using Google.Apis.Auth.AspNetCore3;
+using Google.Apis.Auth.OAuth2;
 using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace ADOPSE_2023.Controllers
 {
-    public class calendarEvent
-    {
-        public string? Summary { get; set; }
-        public string? Organizer { get; set; }
-        public string? Description { get; set; }
-        public string? StartTime { get; set; }
-        public string? EndTime { get; set; }
-    }
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -33,54 +30,77 @@ namespace ADOPSE_2023.Controllers
             return View();
         }
 
-        public List<calendarEvent> GoogleEvents = new List<calendarEvent>();
-        //static string[] scopes = { CalendarService.Scope.CalendarReadonly };
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "ADOPSE-2023";
 
         public IActionResult HomePage()
         {
-            API();
-            ViewBag.EventList = GoogleEvents;
-            return View();
-        }
-
-        public void API()
-        {
-            // Authenticate using OAuth 2.0
             UserCredential credential;
+
             using (var stream = new FileStream("Credential.json", FileMode.Open, FileAccess.Read))
             {
                 string credPath = "token.json";
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.FromStream(stream).Secrets,
-                    new[] { CalendarService.Scope.Calendar },
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
                     "user",
                     CancellationToken.None,
                     new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
             }
 
-            // Create the calendar service
+            // Create Google Calendar API service.
             var service = new CalendarService(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
-                ApplicationName = "Ogma"
+                ApplicationName = ApplicationName,
             });
 
-            // Query the calendar events
-            var events = service.Events.List("primary").Execute().Items;
+            // Define parameters for the request.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
 
-            // Display the calendar events in your web app
-            foreach (var evt in events)
+            // Execute the request and retrieve the events.
+            Events events = request.Execute();
+
+            List<GoogleEvent> upcomingEvents = new List<GoogleEvent>();
+
+            if (events.Items != null && events.Items.Count > 0)
             {
-                // Display the event details (title, start time, end time, etc.)
-                var calendarevt = new calendarEvent();
-                calendarevt.Organizer = evt.Organizer.Email;
-                calendarevt.Summary = evt.Summary;
-                calendarevt.StartTime = evt.Start.DateTimeRaw.ToString();
-                calendarevt.EndTime = evt.End.DateTimeRaw.ToString();
-                calendarevt.Description = evt.Description;
-                GoogleEvents.Add(calendarevt);
-            }
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
 
+                    GoogleEvent googleEvent = new GoogleEvent
+                    {
+                        Summary = eventItem.Summary,
+                        Description = eventItem.Description,
+                        Organizer = eventItem.Organizer.DisplayName,
+                        Start = eventItem.Start.DateTime,
+                        End = eventItem.End.DateTime
+                    };
+
+                    upcomingEvents.Add(googleEvent);
+                }
+            }
+            return View(upcomingEvents);
+        }
+
+        public class GoogleEvent
+        {
+            public string Summary { get; set; }
+            public string Description { get; set; }
+            public string Organizer { get; set; }
+            public DateTime? Start { get; set; }
+            public DateTime? End { get; set; }
         }
     }
 }
