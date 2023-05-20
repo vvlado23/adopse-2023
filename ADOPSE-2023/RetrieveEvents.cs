1bf234163e560abc836f94a06dd7017b083d7d29
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2023-05-20 21:22:12 +0300

    fetching events

---
+++ ADOPSE-2023/RetrieveEvents.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient.Authentication;
+
+namespace ADOPSE_2023
+{
+    public class RetrieveEvents
+    {
+        public void getEvents()
+        {
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            string query = "SELECT * FROM event";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                Console.WriteLine(rd.GetString(0));
+            }
+            rd.Close();
+            conn.Close();
+        }
+    }
+}
