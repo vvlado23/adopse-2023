fe64b615b3ffe9bc7df0638be373b6b6ec362ec9
DimitrisToumpas <dimitristoumpas99@gmail.com>
2023-05-20 18:14:32 +0300

    add module

--- ADOPSE-2023/Module.cs
+++ ADOPSE-2023/Module.cs
@@ -1,19 +1,25 @@
 ﻿
-    public class Module
-    {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public string Price { get; set; }
-            public string Difficulty { get; set; }
-            public int Rating { get; set; }
+public class Module
+{
+   // public int Id { get; set; }
+   // public string Name { get; set; }
+    public string Price { get; set; }
+   // public string Difficulty { get; set; }
+    public int Rating { get; set; }
+    public int idModules { get; set; }
+    public string moduleName { get; set; }
+    public string moduleDesc { get; set; }
 
-            public Module(int id, string name, string price, string difficulty, int rating)
-            {
-                Id = id;
-                Name = name;
-                Price = price;
-                Difficulty = difficulty;
-                Rating = rating;
-            }
-        }
-    
+    public Module(int idModules, string moduleName, string price, int rating, string moduleDesc)
+    {
+        //Id = id;
+        //Name = name;
+        Price = price;
+        //Difficulty = difficulty;
+        Rating = rating;
+        this.idModules = idModules;
+        this.moduleName = moduleName;
+        this.moduleDesc = moduleDesc;
+    }
+}
+
