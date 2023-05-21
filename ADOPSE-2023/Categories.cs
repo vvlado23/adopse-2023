6c540d1a12494efe74f726a52d0a5bad54fd3fd2
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2023-05-21 18:38:59 +0300

    categories

---
+++ ADOPSE-2023/Categories.cs
@@ -0,0 +1,16 @@
+﻿namespace ADOPSE_2023
+{
+    public class Categories
+    {
+        public int idCategory { get; set; }
+        public string CategoryName { get; set; } 
+        public int ParentCategoryID { get; set; }
+
+        public Categories(int idCategory, string categoryName, int ParentCategoryID)
+        {
+            this.idCategory = idCategory;
+            this.CategoryName = categoryName;
+            this.ParentCategoryID = ParentCategoryID;
+        }
+    }
+}
