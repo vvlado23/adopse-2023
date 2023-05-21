5f969633dd4e4b5ac3e151395a9662afe58b5226
DimitrisToumpas <dimitristoumpas99@gmail.com>
2023-05-21 23:50:14 +0300

    fetchdata

---
+++ ADOPSE-2023/FetchData.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ADOPSE_2023
+{
+    
+    public class FetchData
+    {
+        string connectionString = "server=it154484.mysql.database.azure.com;database=it185158;uid=it185158;password=Ogma123!;";
+        
+        
+
+        public FetchData()
+        {
+            this.connectionString = connectionString;
+        }
+
+        public DataTable FetchDataFromTable()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = $"SELECT modulecategories.idCategory, modulecategories.idModule, categories.CategoryName FROM modulecategories JOIN categories ON modulecategories.idCategory = categories.idCategory";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(command.ExecuteReader());
+                    return dataTable;
+                }
+            }
+        }
+    }
+}
--- ADOPSE-2023/Program.cs
+++ ADOPSE-2023/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using Lucene.Net.Store;
 using ADOPSE_2023;
+using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,25 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=HomePage}/{id?}");
 
 app.MapRazorPages();
+
+
+
+//string connectionString = "server=it154484.mysql.database.azure.com;database=it185158;uid=it185158;password=Ogma123!;";
+
+FetchData dataFetcher = new FetchData();
+DataTable dataTable = dataFetcher.FetchDataFromTable();
+
+// Process the fetched data
+foreach (DataRow row in dataTable.Rows)
+{
+    int idCategory = (int)row["idCategory"];
+    int idModule = (int)row["idModule"];
+    string categoryName = row["CategoryName"].ToString();
+
+    // Display the fetched data on the console
+    Console.WriteLine($"Category ID: {idCategory}, Module ID: {idModule}, Category Name: {categoryName}");
+}
+
 
 app.Run();
 
